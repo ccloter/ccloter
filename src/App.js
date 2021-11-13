9760bc1a23906660f5eb9a295702673e6d5eb50e
ccloter <ccloter@gmail.com>
2021-11-12 17:41:52 -0800

    'React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Tweet from './tweet';
 
 function App() {
+  const [users, setUser] = useState([
+    { name: ' Carson', message: "Whats up", likes: '1,000,000' },
+    { name: 'fathead', message: "hello mate", likes: '10,000' }
+  ])
+  const [isRed, setRed] = useState(false)
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map(users => (
+        <Tweet name={users.name} message={users.message} likes={users.likes} />
+      ))}
+
+
+
+      {/* <Tweet name="CLout" message='I finally did it!' likes="1,000" />
+      <Tweet name="Bolt" message='My fart stinks' likes="100,000" />
+      <Tweet name="Ace" message='How can I be annoying today' likes="-9,000" />
+      <Tweet name="Cars" message='fast FaSt FAST' likes="800" /> */}
+
+      <h1 className={isRed ? 'red' : ""}>Change the color</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
     </div>
   );
 }
